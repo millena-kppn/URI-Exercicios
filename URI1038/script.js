c18662650b9bc87dc19d4e348ba218926ba169ca
Millena Kappaun <164417128+millena-kppn@users.noreply.github.com>
2025-04-18 15:48:54 -0300

    desenvolvendo atvdd 1038

---
+++ URI1038/script.js
@@ -0,0 +1,9 @@
+"use strict"
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+// let lanche = parseInt(lines[0])
+// let quantidadeDeLanche = parseInt(lines[1])
+// let totalComanda = Number(lanche) * Number(quantidadeDeLanche)
+// console.log(`Total: R$${totalComanda.toFixed(2)}`)
+
