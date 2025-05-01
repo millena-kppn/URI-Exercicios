0a26ed34d6526ee47e3782bf7defa37c3ac20122
Millena Kappaun <kappaunmillena@gmail.com>
2025-04-30 21:16:39 -0300

    desenvolvendo 1038

--- URI1038/script.js
+++ URI1038/script.js
@@ -2,8 +2,20 @@
 var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 var lines = input.split('\n');
 
-// let lanche = parseInt(lines[0])
-// let quantidadeDeLanche = parseInt(lines[1])
-// let totalComanda = Number(lanche) * Number(quantidadeDeLanche)
-// console.log(`Total: R$${totalComanda.toFixed(2)}`)
+let produto = lines[0].split(' ') [0] //pego minha linha separo o valor por um espaço 
+let qauntidadeDeProduto = lines[0].split(' ')[1] //e acesso cada valor no Array através da posição
+let precoDoLanche = 0 // criar ela fora para poder acessar em diferentes escopos e fazer a troca de valor 
+    if(produto == 1){
+        precoDoLanche = 4.00
+    }else if(produto == 2){
+        precoDoLanche = 4.50
+    }else if(produto == 3){
+        precoDoLanche = 5.00
+    }else if(produto == 4){
+        precoDoLanche == 2.00
+    }else{
+        precoDoLanche == 1.50
+    }
+    let total = produto * precoDoLanche
+    console.log(`Total: R$ ${total.toFixed(2)}`)
 
